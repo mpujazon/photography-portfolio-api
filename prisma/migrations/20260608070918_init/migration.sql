5de39fdcd3aac2188e7cc4b405b4282340942d28
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-06-08 09:11:06 +0200

    PHO-8. Prisma has been configured, connected with DB and creates the Photo model

---
+++ prisma/migrations/20260608070918_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Photo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Photo_pkey" PRIMARY KEY ("id")
+);

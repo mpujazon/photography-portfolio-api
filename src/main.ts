6011d19b6e878d54ac284fa6c9fb1510545e0574
mpujazon <mpujazoncardenas@gmail.com>
2026-08-19 23:22:59 +0200

    fix: migration fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import dns from 'node:dns';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  dns.setDefaultResultOrder('ipv4first');
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:5173', 'https://lensbymike.studio', 'https://www.lensbymike.studio'],
